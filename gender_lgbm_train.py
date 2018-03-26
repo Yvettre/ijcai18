1ff24fd0eec8e418fe0d41ae50e9a7a91716e1e6
dingyf <ding_yanfang@foxmail.com>
2018-03-26 10:28:32 +0800

    写好分性别模型的submit函数

--- gender_lgbm_train.py
+++ gender_lgbm_train.py
@@ -42,6 +42,9 @@ val_df['context_page_id_8'] = (val_df['context_page_id'] <= 8)
 # test_df[drop_list] = 0
 # val_df[drop_list] = 0
 
+p_female = 0.5
+p_male = 0.5
+
 params = {
     'task': 'train',
     'boosting': 'gbdt',
@@ -68,8 +71,15 @@ def train():
     y_pred_train = []
     y_pred_val = []
 
-    # 分四个性别模型训练
-    for gender in xrange(-1,3):
+    val_loss_special = []
+    y_pred_special = []
+
+    val_df_special = val_df[(val_df['user_gender_id']==-1) | (val_df['user_gender_id']==2)]
+    label_val_special = val_df_special['is_trade'].values
+    data_val_special = val_df_special.drop(['user_gender_id', 'instance_id', 'is_trade'], axis=1).values
+
+    # 分两个性别模型训练
+    for gender in xrange(0,2):
         print 'Now training for gender=', gender
 
         train_df_gender = train_df[train_df['user_gender_id']==gender]
@@ -108,6 +118,10 @@ def train():
         val_loss.append(log_loss(label_val, y_pred))
         y_pred_val.append(np.hstack((label_val[:,None], y_pred[:, None])))
 
+        y_pred = gbm.predict(data_val_special, num_iteration=gbm.best_iteration)
+        val_loss_special.append(log_loss(label_val_special, y_pred))
+        y_pred_special.append(y_pred)
+
         joblib.dump(gbm, 'model/gbm_gender'+str(gender))
         print 'train_loss: {:4}, val_loss: {:4}'.format(train_loss, val_loss)
         feature_importance_file = 'feature_importance_'+str(gender)+'.txt'
@@ -124,9 +138,21 @@ def train():
     
     print 'gender_lgbm_train is done!'
     print '-------------------------------------'
-    for gender in xrange(-1,3):
+    for gender in xrange(0,2):
         print 'gender:', gender
-        print 'train_loss: {:4}, val_loss: {:4}'.format(train_loss[gender+1], val_loss[gender+1])
+        print 'train_loss: {:4}, val_loss: {:4}'.format(train_loss[gender], val_loss[gender])
+    print '-------------------------------------'
+    print 'gender: -1 & 2'
+    print 'for model-female:'
+    print 'val_loss: {:4}'.format(val_loss_special[0])
+    print 'for model-male:'
+    print 'val_loss: {:4}'.format(val_loss_special[1])
+    print 'for {:2} * female + {:2} * male:'.format(p_female, p_male)
+    print 'val_loss: {:4}'.format(0.5*val_loss_special[0]+0.5*val_loss_special[1])
+    print '-------------------------------------'
+    tmp = p_female*y_pred_special[0] + p_male*y_pred_special[1]
+    y_pred_val.append(np.hstack((label_val_special[:,None], tmp[:, None])))
+
     train_all = np.vstack(y_pred_train)
     val_all = np.vstack(y_pred_val)
     train_logloss_all = log_loss(train_all[:,0], train_all[:,1])
@@ -136,31 +162,48 @@ def train():
 
     
 def submit():
-    result_list = []
-    for gender in xrange(-1,3):
-        print 'Now testing for gender=', gender
+    result = pd.read_csv('data/round1_ijcai_18_test_a_20180301.txt', sep=' ')
+    result = result[['instance_id']]
+    df_tmp = None
+    y_special = []
+    test_df_special = test_df[(test_df['user_gender_id']==-1) | (test_df['user_gender_id']==2)]
+    id_test_special = test_df_special['instance_id'].values
+    data_test_special = test_df_special.drop(['user_gender_id', 'instance_id'], axis=1).values
+
+    for gender in xrange(0,2):
+        print 'Now for model-gender', gender
 
         test_df_gender = test_df[test_df['user_gender_id']==gender]
+        id_test_gender = test_df_gender['instance_id'].values
+        data_test_gender = test_df_gender.drop(['user_gender_id', 'instance_id'], axis=1).values
+        print '{}:{}'.format(gender, len(id_test_gender))
+        gbm = joblib.load('model/gbm_gender'+str(gender))
 
-        id_test = test_df_gender['instance_id']
-        data_test = test_df.drop(['user_gender_id', 'instance_id'], axis=1).values
-
-        model_file = 'model/gbm_gender'+str(gender)
-        gbm = joblib.load(model_file)
-
-        y = gbm.predict(data_test, num_iteration=gbm.best_iteration)
+        y = gbm.predict(data_test_gender, num_iteration=gbm.best_iteration)
+        y_special.append(gbm.predict(data_test_special, num_iteration=gbm.best_iteration))
 
         y = pd.Series(y, name='predicted_score')
-        result_list.append(pd.concat([id_test, y], axis=1))
+        id_test_gender = pd.Series(id_test_gender, name='instance_id')
+        tmp = pd.concat([id_test_gender, y], axis=1)
+        if df_tmp is None:
+            df_tmp = tmp
+        else:
+            df_tmp = pd.concat([df_tmp, tmp], axis=0)
 
-    result = pd.concat(result_list, axis=0)
+    y = p_female*y_special[0] + p_male*y_special[1]
+    y = pd.Series(y, name='predicted_score')
+    id_test_special = pd.Series(id_test_special, name='instance_id')
+    tmp = pd.concat([id_test_special, y], axis=1)
+    df_tmp = pd.concat([df_tmp, tmp], axis=0)
+    result = pd.merge(result, df_tmp, how='left', on='instance_id')
+
     time_format = '%Y-%m-%d-%H-%M-%S'
     time_now = datetime.datetime.now()
     bak_file = 'result/result_%s.csv'%time_now.strftime(time_format)
     result.to_csv(bak_file, index=False, sep=' ', mode='wb') # for backup
     result.to_csv('result/result.csv', index=False, sep=' ', mode='wb')
     print bak_file
-    print y.mean()
+    print result['predicted_score'].values.mean()
 
 
 def main():
